cf3033ae4c76eb876f4765c414913d65a3513ca7
evgen <1212evgen@gmail.com>
2020-11-14 11:41:04 +0600

    Updates

---
+++ precache-manifest.65e46fcc70d8726fe120ee4986e4cb44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31576211bc4c9719f32829d163534889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "856120e9fb989d9e95e9",
+    "url": "/static/css/2.c3bf1e3c.chunk.css"
+  },
+  {
+    "revision": "ed898f5bb6743b3ae4d8",
+    "url": "/static/css/main.4aac27c4.chunk.css"
+  },
+  {
+    "revision": "856120e9fb989d9e95e9",
+    "url": "/static/js/2.a214fc4d.chunk.js"
+  },
+  {
+    "revision": "67250b032f07f9a0b1abb3a9fa61b025",
+    "url": "/static/js/2.a214fc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02c47e65112b29fe4ab",
+    "url": "/static/js/3.5f621025.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.5f621025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925d212b9e2d8895efe1",
+    "url": "/static/js/4.0c8777d4.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.0c8777d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcae595ad62797b1c16",
+    "url": "/static/js/5.c2e2cbd3.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.c2e2cbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed898f5bb6743b3ae4d8",
+    "url": "/static/js/main.aab735c2.chunk.js"
+  },
+  {
+    "revision": "52170985197ce5ab8e65",
+    "url": "/static/js/runtime-main.0a385e87.js"
+  },
+  {
+    "revision": "99f799550bf3552e8ca8e58b41ccc38a",
+    "url": "/static/media/Circe-Regular.99f79955.woff2"
+  },
+  {
+    "revision": "d7c79911d79b6c6577d6808902d0c0e6",
+    "url": "/static/media/logo.d7c79911.png"
+  }
+]);
